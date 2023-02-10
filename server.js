707b9b43fdb075bbb1f329c153c78c1ed14b8e1e
Kenson Johnson <kenson.johnson@outlook.com>
2023-02-09 21:27:42 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,44 @@
+import express from "express";
+import articleRouter from "./routes/articles.js";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import Article from "./models/Article.js";
+import methodOverride from "method-override";
+
+dotenv.config();
+
+// Create the express app
+const app = express();
+
+// Connect to MongoDB Atlas
+mongoose.set("strictQuery", false);
+const mongoDB = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER_NAME}.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
+
+main().catch((err) => console.log(err));
+async function main() {
+  mongoose.connect(mongoDB, () => {
+    console.log("Connected to DB");
+  });
+}
+
+// ---------- Configure express ---------- //
+// Sets the view engine to ejs
+app.set("view engine", "ejs");
+// Let's us use data from within the request body
+app.use(express.urlencoded({ extended: false }));
+// Let's us use methods other than just POST or GET
+app.use(methodOverride("_method"));
+
+// Setting up routes and routers
+app.use("/articles", articleRouter);
+
+app.get("/", async (req, res) => {
+  const articles = await Article.find().sort({
+    createdAt: "desc",
+  });
+  res.render("articles/index", { articles });
+});
+
+const PORT = process.env.PORT || 5050;
+
+app.listen(PORT);
